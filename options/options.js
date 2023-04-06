a48ad2e28717a5cb05dba17e52ba315a83185019
Crowdbotics <engineering@crowdbotics.com>
2023-04-06 00:07:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muddy-poetry-dev-74687",
+  url: "https://muddy-poetry-dev-74687.botics.co",
+  api: "https://muddy-poetry-dev-74687.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
